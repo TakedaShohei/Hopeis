b8e27d45c7f70001301a3cf066e21d9a197e0b75
Themyscira <takeda.syouhei@gmail.com>
2020-05-16 00:30:03 +0900

    making item manager and game manager

---
+++ HopeIs/Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum GameState
+{
+    Start,
+    Prepare,
+    Playing,
+    End
+}
+
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    // 現在の状態
+    private GameState currentGameState;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
