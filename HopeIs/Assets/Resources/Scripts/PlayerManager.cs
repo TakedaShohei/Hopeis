52a23390eb3800298e408d42e33fe3e7176e0c9c
Themyscira <takeda.syouhei@gmail.com>
2020-05-16 15:29:22 +0900

    finished PlayerManager

---
+++ HopeIs/Assets/Resources/Scripts/PlayerManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed = 10.0f;
+    
+    // Use this for initialization
+    void Start()
+    {
+        // 回転しない設定
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey("up"))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+            
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.position += transform.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.position -= transform.right * speed * Time.deltaTime;
+        }
+    }
+}
