3850d8cc8dd4756ddea3fc8fe073f5348bc4d668
Themyscira <takeda.syouhei@gmail.com>
2020-05-18 23:51:14 +0900

    making item manager

--- HopeIs/Assets/Resources/Scripts/GameManager.cs
+++ HopeIs/Assets/Resources/Scripts/GameManager.cs
@@ -2,44 +2,70 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-public enum GameState
-{
-    Start,
-    Prepare,
-    Playing,
-    End
-}
-
+using System.Linq;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager instance_;
-    PlayerManager player_manager_ = null;
-    // 現在の状態
-    private GameState currentGameState;
-    public GameObject[] object_ =new GameObject[3];
-    public GameObject[] object_count_ = GameObject.FindGameObjectsWithTag("item");
-    public int count_=0;
+    public enum GameState
+    {
+        Start,
+        Prepare,
+        Playing,
+        End
+    }
+
+    GameState current_state_;
+    public GameState CurrentState
+    {
+        get { return current_state_; }
+        set { current_state_ = value; }
+    }
+
+    [SerializeField] private int item_count_;
+    public int ItemCount
+    {
+        get { return item_count_; }
+        protected set { item_count_ = value; }
+    }
+
+    List<GameObject> item_list_ = new List<GameObject>();
+
+    
+   
+    //public GameObject[] object_count_=null;
+    [SerializeField] GameObject goal_transform_ = null;
+    
+
+    
+
 
     // Use this for initialization
     void Start()
     {
+       
+       
 
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        
+        
+            goal_transform_.SetActive(true);
+        
     }
 
     void Setup()
     {
-        if (count_ > 2)
-        {
+        current_state_ = GameState.Start;
+        item_count_ = item_list_.Count();
+        goal_transform_.SetActive(false);
+       
+    }
 
-        }
+    public void AddItem(GameObject item_)
+    {
 
     }
 
--- HopeIs/Assets/Resources/Scripts/ItemManager.cs
+++ HopeIs/Assets/Resources/Scripts/ItemManager.cs
@@ -1,23 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ItemManager : GameManager
-{
+public class ItemManager : MonoBehaviour
+{ 
     [SerializeField] GameObject item_ = null;
-    public void GetItem()
+    // トリガーとの接触時に呼ばれるコールバック
+    GameManager game_manager_ = null;
+    void OnTriggerEnter(Collider hit)
     {
-        count_++;
-        
-        Debug.Log(count_);
-        //Debug.Log();
-        item_.SetActive(false);
+        // 接触対象はPlayerタグですか？
+        if (hit.CompareTag("Player"))
+        {
+            // object_count_ = GameObject.FindGameObjectsWithTag("item");
+            // Debug.Log(object_count_.Length);
+            game_manager_.AddItem(item_);
+            Debug.Log(game_manager_.ItemCount);
 
-       
+            item_.SetActive(false);
+
+        }
     }
     // Use this for initialization
     void Start()
     {
-
+        game_manager_ = new GameManager();
     }
 
     // Update is called once per frame
